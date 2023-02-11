23be0a8e1df695a32903f3cb3acc39c7c2eed252
Aditya Kumar <adityakumar9939@gmail.com>
2023-02-12 00:35:33 +0530

    used arrow function

--- 1st/script.js
+++ 1st/script.js
@@ -1,10 +1,8 @@
-let index = 0;
-
-function changeColor() {
-    let colors = ["red","blue","orange","yellow","green","purple","violet"];
-    document.getElementsByTagName("body")[0].style.background = colors[index++];
-
-    if(index > colors.length - 1) {
-        index = 0;
+let count = 0;
+changeColor = () => {
+    let colors = ["blue", "red", "green", "violet", "purple"];
+    document.getElementsByTagName("body")[0].style.background = colors[count++];
+    if (count > colors.length - 1) {
+        count = 0;
     }
 }
